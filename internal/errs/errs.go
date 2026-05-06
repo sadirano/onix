41586319a23e99da3ebe43f8261f32dcb3ad87ca
Sadirano <leonardofariasbona@gmail.com>
2026-05-06 02:52:34 -0300

    errs: add exit code constants and Fatal/FatalCode helpers

--- internal/errs/errs.go
+++ internal/errs/errs.go
@@ -1,7 +1,18 @@
 // Package errs defines shared error types used across onix internal packages.
 package errs
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+// Exit codes used by onix.
+const (
+	ExitOK       = 0
+	ExitErr      = 1 // general error
+	ExitUsage    = 2 // bad arguments / usage error
+	ExitNotFound = 3 // alias or module not found
+)
 
 // ExitError is returned when a child process exits with a non-zero code.
 // main.go checks for this type and calls os.Exit with the embedded code,
@@ -12,4 +23,15 @@ type ExitError struct {
 
 func (e *ExitError) Error() string {
 	return fmt.Sprintf("exit status %d", e.Code)
+}
+
+// Fatal prints an error to stderr and exits with ExitErr.
+func Fatal(format string, a ...any) {
+	FatalCode(ExitErr, format, a...)
+}
+
+// FatalCode prints an error to stderr and exits with the given code.
+func FatalCode(code int, format string, a ...any) {
+	fmt.Fprintf(os.Stderr, "onix: "+format+"\n", a...)
+	os.Exit(code)
 }
